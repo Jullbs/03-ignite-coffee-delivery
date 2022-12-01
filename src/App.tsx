16bb3d3c4e89c3a4bfdaaa17c8db793a175828f7
Juliana Daroz <julianadaroz@gmail.com>
2022-12-01 12:22:05 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// STYLE
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Coffee</h1>
+    </ThemeProvider>
+  )
+}
